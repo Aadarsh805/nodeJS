5c039d36d6a6c2d2104676dce30d635400b0967e
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-08-25 23:33:37 +0530

    http

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const path = require('path')
+const http = require("http");
 
-console.log(path.sep)
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end('home page bois')
+    }
+
+    if(req.url === '/about'){
+        res.end('about page bois')
+    }
+
+    res.end(`
+        <h1>no such page exits</h1>
+        <a href='/'>go back to home page<a>
+    `)
+    
+  res.write("Harro harro harro");
+  res.end()
+});
+
+server.listen(5000);
